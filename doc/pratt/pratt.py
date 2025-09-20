f5a9d127bdc35ab74a2218aa796a8cfaa45cab0e
markus-zhang <markus_zhang@hotmail.com>
2025-09-19 21:56:49 -0400

    Gonna practice Pratt in Python

---
+++ doc/pratt/pratt.py
@@ -0,0 +1,20 @@
+"""
+    A research script that demonstrates Pratt's parser:
+
+    1. Very simple language
+    - all tokens separated by whitespaces, even strings (no space allowed in strings, lol), so that I can simply call split()
+    - Only supports float numbers
+    - Operators:
+        - Unary (only OP_NEG)
+        - Binary(OP_ADD, OP_SUB, OP_MUL, OP_DIV)
+        - Grouping
+
+    2. REPL only, not scripts import allowed
+    - And only allow a single line of input :)
+"""
+
+def repl():
+    
+
+if __name__ == '__main__':
+    repl()
